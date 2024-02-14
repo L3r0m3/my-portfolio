db9495da0364f58ac593ac371f5091cbcb08789c
L3r0m3 <lerome_brown@yahoo.com>
2024-02-14 18:14:35 +0100

    comments

--- components/header.tsx
+++ components/header.tsx
@@ -7,9 +7,9 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import { useActiveSectionContext } from '@/context/active-section-context';
 
-// Component function
+// {Component function}
 export default function Header() {
-    // Get the active section from the context
+    // {Get the active section from the context}
    const {activeSection, setActiveSection, setTimeOfLastClick} = useActiveSectionContext()
 
   return (
@@ -29,7 +29,7 @@ export default function Header() {
             sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
             <ul className='flex flex-wrap w-[22rem] items-center justify-center gap-y-1
              text-gray-500 text-[0.9rem] font-medium sm:w-[initial] sm:flex-nowrap sm:gap-5'>
-                {links.map((link) => ( // Create a list item for each link
+                {links.map((link) => ( // {Create a list item for each link}
                     <motion.li className='h-3/4
                         flex items-center
                         justify-center relative'
@@ -37,7 +37,7 @@ export default function Header() {
                         initial={{ y: -100, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
                     >
-                        <Link // Create a link to the link's hash
+                        <Link // {Create a link to the link's hash}
                             className={clsx(
                             'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
                             {
@@ -46,7 +46,7 @@ export default function Header() {
                             }
                             )}
                             href={link.hash}
-                            // Set the active section to the link's name
+                            // {Set the active section to the link's name}
                             onClick={() => {
                                 setActiveSection(link.name)
                                 setTimeOfLastClick(Date.now)
