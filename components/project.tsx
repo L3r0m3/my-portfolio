f2e0e4c330af7ec294ff7d6ef10b26052b765176
L3r0m3 <lerome_brown@yahoo.com>
2024-08-11 10:34:34 +0200

    chore:

--- components/project.tsx
+++ components/project.tsx
@@ -13,6 +13,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  siteUrl,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -33,59 +34,58 @@ export default function Project({
     >
       <section
         className="
-          bg-gray-100 max-w-[42rem]
-          border border-black/5 rounded-lg
-          overflow-hidden sm:pr-8
-          relative sm:h-[20rem]
-          sm:group-even:pl-8 hover:bg-gray-200 transition
-          dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
+        bg-gray-100 max-w-[42rem]
+        border border-black/5 rounded-lg
+        overflow-hidden sm:pr-8
+        relative sm:h-[20rem]
+        sm:group-even:pl-8 hover:bg-gray-200 transition
+        dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
       >
-        <div
-          className="
+        <Link href={siteUrl}>
+          <div
+            className="
                 pt-4 pb-7 px-5 sm:pl-10
                 sm:pr-2 sm:pt-10
                 sm:max-w-[50%] flex
                 flex-col h-full
                 sm:group-even:ml-[18rem]"
-        >
-          <h3
-            className="
-                  text-2xl font-semibold"
           >
-            {title}
-          </h3>
-          <p
-            className="
+            <h3
+              className="
+                  text-2xl font-semibold"
+            >
+              {title}
+            </h3>
+            <p
+              className="
                   mt-2 leading-relaxed
                   text-gray-700 dark:text-white/70"
-          >
-            {description}
-          </p>
-          <ul
-            className="
+            >
+              {description}
+            </p>
+            <ul
+              className="
                   flex flex-wrap
                   mt-4 gap-2 sm:mt-auto"
-          >
-            {tags.map((tag, index) => (
-              <li
-                className="
+            >
+              {tags.map((tag, index) => (
+                <li
+                  className="
                       bg-black/[0.7] px-3 py-1
                       text-[0.7rem] uppercase
                       tracking-wider text-white
                       rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-          {projectsData.map((urlData, index) => (
-            <Link key={index} href={urlData.siteUrl}>
-              <Image
-                src={imageUrl}
-                alt="Project I worked on"
-                quality={95}
-                className="
+                  key={index}
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+            <Image
+              src={imageUrl}
+              alt="Project I worked on"
+              quality={95}
+              className="
                   absolute hidden sm:block top-8 -right-40
                   w-[28.25rem] rounded-t-lg
                   shadow-2xl
@@ -101,10 +101,9 @@ export default function Project({
                   
                   group-even:right-[initial]
                   group-even:-left-40"
-              />
-            </Link>
-          ))}
-        </div>
+            />
+          </div>
+        </Link>
       </section>
     </motion.div>
   );
