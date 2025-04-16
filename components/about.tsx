d731101fd2de94dd261df9bcdedfbcc25f38269c
L3r0m3 <lerome_brown@yahoo.com>
2025-04-16 17:22:44 +0200

    some more corrections

--- components/about.tsx
+++ components/about.tsx
@@ -1,42 +1,55 @@
 // use client side rendering
-'use client';
+"use client";
 
 // Import required React components and hooks
-import React from 'react'
-import { motion } from 'framer-motion';
-import SectionHeading from './section-heading';
-import { useSectionInView } from '@/lib/hooks';
+import React from "react";
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
 
 // Component function
 export default function About() {
-  const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.section
-        ref={ref}
-        className='
+      ref={ref}
+      className="
         mb-28 max-w-[45rem]
         text-center leading-8
-        sm:mb-40 scroll-mt-28'
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.175 }}
-        id='about'>
-        <SectionHeading>About me</SectionHeading>
-        <p className='mb-3'>
-            I'm a <span className='font-medium'>Junior Full-Stack Developer</span>{' '}
-            with a <span className='font-medium'>Meta Frontend Developer Certification</span>,{' '}
-            <span className='italic'>driven by a deep passion for programming and problem-solving.</span> {' '}
-            I'm constantly motivated to embrace new technologies and expand my skill set.
-        </p>
-        <p>
-            Beyond coding, I find balance in life through activities like jogging, Muay Thai and meditation.{' '}
-            <span className='underline'>I thrive</span> on turning{' '}
-            <span className='font-medium'>ideas into functional and user-friendly applications</span> {' '}
-            and am <span className='italic'>always eager to take on new challenges</span>.{' '}
-            <span className='underline'>Join me</span> on my journey as I explore
-            the ever-evolving world of technology and continue to grow as a developer.
-        </p>
+        sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I'm a <span className="font-medium">Junior Full-Stack Developer</span>{" "}
+        with a{" "}
+        <span className="font-medium">
+          Meta Frontend Developer Certification
+        </span>
+        ,{" "}
+        <span className="italic">
+          driven by a deep passion for programming and problem-solving.
+        </span>{" "}
+        I'm constantly motivated to embrace new technologies and expand my skill
+        set.
+      </p>
+      <p>
+        Beyond coding, I find balance in life through activities like jogging,
+        Muay Thai and meditation. <span className="underline">I thrive</span> on
+        turning{" "}
+        <span className="font-medium">
+          ideas into functional and user-friendly applications
+        </span>{" "}
+        and am{" "}
+        <span className="italic">always eager to take on new challenges</span>.{" "}
+        <span className="underline">Join me</span> on my journey as I explore
+        the ever-evolving world of technology and continue to grow as a
+        developer.
+      </p>
     </motion.section>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -75,8 +75,8 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Lerome.</span> I'm a{" "}
         <span className="font-bold">Junior Full-Stack Developer</span> with{" "}
-        <span className="font-bold">1.5 years</span> of experience. I enjoy
-        building <span className="italic">sites</span>. My focus is{" "}
+        <span className="font-bold">6 Months</span> of working experience. I
+        enjoy building <span className="italic">sites</span>. My focus is{" "}
         <span className="underline">React(Next.js)</span>.
       </motion.h1>
 
