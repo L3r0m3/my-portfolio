520b11384fd36411b97aa2911e5f84d3a1def90b
L3r0m3 <lerome_brown@yahoo.com>
2025-07-16 21:56:26 +0200

    fix: useFormstatus import

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -3,7 +3,7 @@
 import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from '@/email/contact-form-email';
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -29,7 +29,7 @@ export const sendEmail = async (formData: FormData) => {
       from: "Contact Form <onboarding@resend.dev>",
       to: "lerome_brown@yahoo.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
